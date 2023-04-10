8010d65cec5f6aee9864971c8ee84aaef69c80d9
hafsa ajaz <hafsaajaz12@gmail.com>
2023-04-10 11:39:12 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,78 @@
+<script type="text/javascript">
+ var myname = 'Ahmed';
+ </script>
+  var age = 27;
+  age = 10;
+  age = 45;
+  age = 18;
+  alert(age);
+  var fnam, lname, age, email, location;
+ var age=18, email='yourname@gmail.com',
+location='Microsoft';
+10 + 5 =15
+ 10 + "5" = 105
+ 8 + 8 + "8" = 168
+ var first_name = 'Mohammad';
+ var last_name = 'Ahmed';
+ alert(first_name + last_name);
+ alert(first_name + " " + last_name);
+ var age = 18;
+ alert('Your age is ' + age);
+ <script type="text/javascript">
+ var my_name = 'Ameen';
+ if(my_name == 'Ameen')
+ {
+ alert('Hi Mr. Ameen');
+ }
+ </script>
+ document.write((2 + 5) - 6 * 3 / 9);
+ document.write(2 + (5 - 6) * 3 / 9);
+ document.write(2 + 5 - (6 * 3) / 9); 
+ var ans = 2 + 5 + 1;
+ var name = 'Mohammad' + 'Ali';
+ var age = 24;
+ if(age < 25)
+ {
+ alert('This is true condition');
+ }
+ else
+ {
+ alert('This is false condition');
+ }
+ var age = 24;
+ if(age > 25)
+ {
+ alert('This is true condition');
+ }
+ else
+ {
+ alert('This is false condition');
+ }
+ var age = 25;
+ if(age <= 25)
+ {
+ alert('This is true condition');
+ }
+ else
+ {
+ alert('This is false condition');
+ }
+ var age = 26;
+ if(age >= 25)
+ {
+ alert('This is true condition');
+ }
+ else
+ {
+ alert('This is false condition');
+ }
+ var age = 26;
+ if(age != 25)
+ {
+ alert('This is true condition');
+ }
+ else
+ {
+ alert('This is false condition');
+ }
+
